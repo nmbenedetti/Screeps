d45cdba6cf3dde799968d4c7f72697cf6826dd83
nmbenedetti <nmb5286@rit.edu>
2016-12-17 19:07:43 -0500

    Comment out mover code

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 var roleHarvester = require('roleHarvester');
 var roleUpgrader = require('roleUpgrader');
 var roleBuilder = require('roleBuilder');
-var roleMover = require('roleMover');
+//var roleMover = require('roleMover');
 
 var NUM_BUILDER = 5;
 var NUM_HARVESTER = 4;
 var NUM_UPGRADER = 3;
-var NUM_MOVER = 1;
+//var NUM_MOVER = 1;
 
 module.exports.loop = function () {
   var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
   var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
   var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-  var movers = _.filter(Game.creeps, (creep) => creep.memory.role == 'mover')
+  //var movers = _.filter(Game.creeps, (creep) => creep.memory.role == 'mover')
 
   //Spawn creeps in order
   if(harvesters.length < NUM_HARVESTER) {
@@ -23,9 +23,9 @@ module.exports.loop = function () {
   }
   else if(upgraders.length < NUM_UPGRADER) {
     var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'upgrader', upgrading: false});
-  }else if (movers.length < NUM_MOVER) {
-      var newName = Game.spawns['Spawn1'].createCreep([CARRY,CARRY,MOVE], undefined, {role: 'mover'});
-  }
+  }//else if (movers.length < NUM_MOVER) {
+  //    var newName = Game.spawns['Spawn1'].createCreep([CARRY,CARRY,MOVE], undefined, {role: 'mover'});
+  //}
 
 
   //Make Creeps perform their roles
