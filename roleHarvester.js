b7a38e32f9847d354b8d5ab7792d7d8dc14bb1de
nmbenedetti <nmb5286@rit.edu>
2016-12-17 21:59:28 -0500

    Fixing Container logic

--- roleHarvester.js
+++ roleHarvester.js
@@ -17,9 +17,14 @@ var roleHarvester = {
       //creep.say("Working!");
       var targets = creep.room.find(FIND_STRUCTURES, {
                   filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_CONTAINER);
+                    return (structure.structureType == STRUCTURE_CONTAINER
+                      && structure.store[RESOURCE_ENERGY] < structure.storeCapacity
+                    );
                   }
           });
+
+          console.log(targets.length);
+
           if(targets.length > 0) {
               if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                   creep.moveTo(targets[0]);
