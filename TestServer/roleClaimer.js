da5b9f1835a3c505e1ea69a459a808142fc84c4e
nmbenedetti <nmb5286@rit.edu>
2017-01-14 12:44:00 -0500

    Revert "Revert "update sim""
    This reverts commit 48207efeaf6a6da44887e8a6b7e511579a8bcbcb.

--- TestServer/roleClaimer.js
+++ TestServer/roleClaimer.js
@@ -1,17 +1,21 @@
-
+var worldManager = require('worldManager');
 var roleClaimer = {
 
   /** @param {Creep} creep **/
-  claimLocation: function(creep,roomName){
+  claimLocation: function(creep){
     if (creep.room.name == creep.memory.targetRoom) {
         creep.moveTo(creep.room.controller);
       if(creep.room.controller && !creep.room.controller.my) {
         if(creep.memory.action == 'claim'){
-          if(creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+          console.log(creep.claimController(creep.room.controller)+' RESULT');
+          var result = creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE
+          if(result == -9) {
               creep.moveTo(creep.room.controller);
+          }else if(result == 0){
+              worldManager.stopClaimRoom(creep.memory.homeRoom, creep.memory.targetRoom);
           }
         }else if (creep.memory.action= 'reserve'){
-          if(creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+          if(creep.reserveController(creep.room.controller) == OK) {
                creep.moveTo(creep.room.controller);
            }
         }
